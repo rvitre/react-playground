bf2abccfdc490313f8ab58d56c6b263d163b7459
Robin Vitre <robin.vitre@webloyalty.ch>
2019-06-25 15:04:50 +0200

    removed favourites because persistor is there now

--- src/js/components/List.js
+++ src/js/components/List.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { deleteArticle, saveArticle, unsaveArticle, saveArticleOk, unsaveArticleOk, } from "../ducks/article";
+import { deleteArticle } from "../ducks/article";
 
 const mapStateToProps = state => {
     return { 
@@ -12,43 +12,19 @@ const mapStateToProps = state => {
 function mapDispatchToProps(dispatch) {
     return {
         deleteArticle: article => dispatch(deleteArticle(article)),
-        saveArticle: article => dispatch(saveArticle(article)),
-        unsaveArticle: article => dispatch(unsaveArticle(article)),
-        saveArticleOk: article => dispatch(saveArticleOk(article)),
-        unsaveArticleOk: article => dispatch(unsaveArticleOk(article)),
     };
 }
 
 
 export class ConnectedList extends Component {
 
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.saveArticleOk) {
-            this.props.saveArticleOk();
-            this.setState({saveArticleOk: false});
-        }
-        if (nextProps.unsaveArticleOk) {
-            this.props.unsaveArticleOk();
-            this.setState({unsaveArticleOk: false});
-        }
-    }
-
     render() {
         return (<ul className="list-group list-group-flush">
-            {this.props.articles.map((el, index) => (
+            {this.props.articles.map((el) => (
                 <li className="list-group-item" key={el.id}>
                     {el.title}
                     &nbsp;
                     <button className="btn btn-danger btn-sm" onClick={() => this.props.deleteArticle(el.id)}>Delete</button>
-                    &nbsp;
-                    <button className="btn btn-success btn-sm" onClick={() => this.props.saveArticle(el)}>Pin</button>
-                </li>
-            ))}
-            {this.props.savedArticles.map((el, index) => (
-                <li className="list-group-item" key={el.id}>
-                    {el.title}
-                    &nbsp;
-                    <button className="btn btn-warning btn-sm" onClick={() => this.props.unsaveArticle(el.id)}>Unpin </button>
                 </li>
             ))}
         </ul>
--- src/js/ducks/article.js
+++ src/js/ducks/article.js
@@ -4,10 +4,6 @@ import { isArray } from "util";
 //-- contants
 export const ADD_ARTICLE = "ADD_ARTICLE";
 export const DELETE_ARTICLE = "DELETE_ARTICLE";
-export const SAVE_ARTICLE = "SAVE_ARTICLE";
-export const UNSAVE_ARTICLE = "UNSAVE_ARTICLE";
-export const SAVE_ARTICLE_OK = "SAVE_ARTICLE_OK";
-export const UNSAVE_ARTICLE_OK = "UNSAVE_ARTICLE_OK";
 export const DATA_REQUESTED = "DATA_REQUESTED";
 export const DATA_LOADED = "DATA_LOADED";
 
@@ -18,19 +14,6 @@ export function addArticle(payload) {
 export function deleteArticle(payload) {
     return { type: DELETE_ARTICLE, payload };
 }
-export function saveArticle(payload) {
-    return { type: SAVE_ARTICLE, payload };
-}
-export function unsaveArticle(payload) {
-    return { type: UNSAVE_ARTICLE, payload };
-}
-export function saveArticleOk(payload) {
-    return { type: SAVE_ARTICLE_OK, payload };
-}
-export function unsaveArticleOk(payload) {
-    return { type: UNSAVE_ARTICLE_OK, payload };
-}
-
 export function getData() {
     return { type: DATA_REQUESTED };
 }
@@ -64,41 +47,6 @@ function reducer(state = initialState, action) {
                 )
             ]
         };
-    }
-    if (action.type === SAVE_ARTICLE) {
-        let isDuplicate = state.savedArticles.some(
-            article => article.id === action.payload.id
-        );
-
-        if (!isDuplicate) {
-            return {
-                ...state,
-                savedArticles: state.savedArticles.concat(action.payload),
-                saveArticleOk: true
-            };
-        } else {
-            return state;
-        }
-    }
-    if (action.type === UNSAVE_ARTICLE) {
-        return {
-            ...state,
-            /*articles: [
-            ...state.articles.slice(0, action.payload),
-            ...state.articles.slice(action.payload + 1)]*/
-            savedArticles: [
-                ...state.savedArticles.filter(
-                    article => article.id !== action.payload
-                )
-            ],
-            unsaveArticleOk: true
-        };
-    }
-    if (action.type === SAVE_ARTICLE_OK) {
-        cookie.save("savedArticles", state.savedArticles);
-    }
-    if (action.type === UNSAVE_ARTICLE_OK) {
-        cookie.save("savedArticles", state.savedArticles);
     }
     if (action.type === DATA_LOADED) {
         return {
